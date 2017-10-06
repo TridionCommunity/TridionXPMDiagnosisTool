0d62bcd6e0615ac2682c4469cc47d49d0ee195dc
Rick Pannekoek <rpannekoek@sdl.com>
2017-10-06 13:27:40 +0200

    Initial version of a PowerShell script to start a XPM Session Preview

---
+++ Start-PreviewSession.ps1
@@ -0,0 +1,36 @@
+﻿Param(
+    [string]$cmsUrl = "http://localhost:81",
+    [string]$pageId = "tcm:5-224-64",  # TODO: support WebDAV URL
+    [string]$pageTemplateId = "tcm:5-182-128"  # TODO: determine from Page (using Core Service)
+)
+
+
+$getPreviewTokenUrl = "$cmsUrl/WebUI/Models/SiteEdit/Services/SessionPreviewService.svc/GetPreviewToken"
+
+$getPreviewTokenHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$getPreviewTokenHeaders.Add("X-SDL-Tridion-Token", '666') # TODO: This should be a genuine token, but this works too (for now)
+
+$publishedItemInfo = @{
+    ItemId = $pageId
+    TemplateId = $pageTemplateId
+    ItemLastModifiedTimeStamp = "/Date(1000000000000)/"  # Intentionally using a date in the past, to ensure it is resolved/rendered
+    ComponentPresentationType = 0
+    }
+
+$getPreviewTokenBody = @{
+    publishedItemsInfo = @($publishedItemInfo)
+    publishingTargetId = "tcm:0-2-65538"
+    }
+
+$getPreviewTokenJson = $getPreviewTokenBody | ConvertTo-Json
+
+$getPreventTokenResponse = Invoke-RestMethod `
+    -Uri $getPreviewTokenUrl `
+    -Method Post `
+    -ContentType "application/json" `
+    -Headers $getPreviewTokenHeaders `
+    -Body $getPreviewTokenJson `
+    -UseDefaultCredentials 
+
+Write-Host "Preview Token:"
+$getPreventTokenResponse.d
